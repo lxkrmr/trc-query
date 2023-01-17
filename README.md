87b7b8514e61b956bc9951c80ae9d892fe732a2b
Alexander Kramer <a.kramer@reply.de>
2023-01-17 21:02:35 +0100

    :sparkles: add endpoints to test query param support for different http verbs

--- README.md
+++ README.md
@@ -6,3 +6,34 @@ Today we learned that the [typed-rest-client](https://github.com/microsoft/typed
 * PATCH
 * PUT
 
+To verify this behaviour one can use this simple Nest.JS application.
+
+1. start the application via `npm run start:dev`
+2. use `requests.http` to fire a request against the GET, POST, PATCH, PUT and DELETE endpoint
+3. verify the response
+
+If type-rest-client supports query params for the given HTTP verb then one will get a response like this:
+
+```JSON
+{
+  "message": "When one does a DELETE request via type-rest-client then the query param is: myNiceQueryParam"
+}
+```
+
+Otherwise, one will see that the query param is undefined:
+
+```JSON
+{
+"message": "When one does a POST request via type-rest-client then the query param is: undefined"
+}
+```
+
+# PS
+
+## good news
+
+The Github user [ajuanjojjj](https://github.com/ajuanjojjj) created a [PR](https://github.com/microsoft/typed-rest-client/pull/301) to support query parameters for POST, PATCH and PUT as well.
+
+## bad news
+
+This PR is open since **Oct 7, 2021**
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,117 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import * as rm from 'typed-rest-client/RestClient';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private client: rm.RestClient;
+
+  constructor() {
+    this.client = new rm.RestClient('my-user-agent', 'http://localhost:3000');
+  }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async get(): Promise<Message> {
+    const response: rm.IRestResponse<Message> = await this.client.get<Message>(
+      '/get-query',
+      this.createQueryParamOptions(),
+    );
+
+    return response.result;
+  }
+
+  @Get('/get-query')
+  getQuery(@Query('param') queryParam: string): Message {
+    return this.createMessage('GET', queryParam);
+  }
+
+  @Post()
+  async post(): Promise<Message> {
+    const response: rm.IRestResponse<Message> =
+      await this.client.create<Message>(
+        '/post-query',
+        this.createQueryParamOptions(),
+      );
+
+    return response.result;
+  }
+
+  @Post('/post-query')
+  postQuery(@Query('param') queryParam: string): Message {
+    return this.createMessage('POST', queryParam);
+  }
+
+  @Patch()
+  async patch(): Promise<Message> {
+    const response: rm.IRestResponse<Message> =
+      await this.client.update<Message>(
+        '/patch-query',
+        this.createQueryParamOptions(),
+      );
+
+    return response.result;
+  }
+
+  @Patch('/patch-query')
+  patchQuery(@Query('param') queryParam: string): Message {
+    return this.createMessage('PATCH', queryParam);
+  }
+
+  @Put()
+  async put(): Promise<Message> {
+    const response: rm.IRestResponse<Message> =
+      await this.client.replace<Message>(
+        '/put-query',
+        this.createQueryParamOptions(),
+      );
+
+    return response.result;
+  }
+
+  @Put('/put-query')
+  putQuery(@Query('param') queryParam: string): Message {
+    return this.createMessage('PUT', queryParam);
+  }
+
+  @Delete()
+  async delete(): Promise<Message> {
+    const response: rm.IRestResponse<Message> = await this.client.del<Message>(
+      '/delete-query',
+      this.createQueryParamOptions(),
+    );
+
+    return response.result;
+  }
+
+  @Delete('/delete-query')
+  deleteQuery(@Query('param') queryParam: string): Message {
+    return this.createMessage('DELETE', queryParam);
+  }
+
+  private createQueryParamOptions() {
+    return {
+      queryParameters: {
+        params: {
+          param: 'myNiceQueryParam',
+        },
+      },
+    };
+  }
+
+  private createMessage(httpVerb: HttpVerb, queryParam: string): Message {
+    return {
+      message: `When one does a ${httpVerb} request via type-rest-client then the query param is: ${queryParam}`,
+    };
   }
 }
+
+type Message = { message: string };
+
+type HttpVerb = 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE';
